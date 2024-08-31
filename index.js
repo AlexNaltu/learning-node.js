34868befc6fadaa69b971d6aab07328da82112b9
Alex Naltu <naltuok@gmail.com>
2024-08-31 08:07:53 +0200

    learned about events emitters

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
+const logEvents = require("./logEvents");
 
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+const EventEmitter = require("events");
 
-console.log(uuid());
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmiter = new MyEmitter();
+
+// add listener for an event
+
+myEmiter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmiter.emit("log", "Log event emitted");
+}, 2000);
